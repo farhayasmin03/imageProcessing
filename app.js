214f3159a645c23a1b07497a840dc85ba198cd7a
Farhana Yasmin <farhayasmin03@gmail.com>
2019-03-26 19:14:02 +0530

    file display

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({
 }));
 //set storage Engine for multer
 var storage = multer.diskStorage({
-    destination: './public/uplodes/',
+    destination: './public/uploads/',
 
     filename: function (req, file, cb) {
         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
@@ -24,12 +24,37 @@ var upload = multer({
 
 app.use(bodyParser.json());
 
-var routes = require('./routes/router');
-app.use('/', routes);
+/* var routes = require('./routes/router');
+app.use('/', routes); */
 
 
 app.use(express.static(__dirname + '/public'));
+app.get("/", function (req, res) {
+    res.render("index");
+});
+app.post('/upload', function (req, res) {
+    upload(req, res, (err) => {
+        if (err) {
+            res.render('index', {
+                msg: err
+            });
+        } else if (req.file == undefined) {
+            res.render('index', {
+                msg: "No file is selected",
 
+            });
+        } else {
+            res.render('index', {
+                msg: "File uploaded",
+                file: `uploads/${req.file.filename}`
+            })
+
+        }
+
+
+    });
+
+});
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
